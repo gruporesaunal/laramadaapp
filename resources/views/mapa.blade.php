0146a953eeb80f5fb05974abbbb2e462f0d69bfd
Santiago Carvajal <scarvajalc@unal.edu.co>
2017-05-15 07:05:49 -0500

    maps view updated, year maps, and information tab added to the view

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -28,7 +28,7 @@ Mapa de la Ramada <div class="hidden-md hidden-lg"></div>  <small>Información d
 			    <ul class="dropdown-menu">
 			      
 			      @foreach($type->pollutants as $pollutant)
-			       <li><a data-toggle="pill" href="#{{$pollutant->name}}">{{$pollutant->name}}</a></li>	   
+			       <li><a data-toggle="pill" href="#poll{{$pollutant->id}}">{{$pollutant->name}}</a></li>	   
                    @endforeach 
 			    </ul>
 			</li>
@@ -37,8 +37,7 @@ Mapa de la Ramada <div class="hidden-md hidden-lg"></div>  <small>Información d
 		 </ul>
     	</div>
 	    <div class="col-md-9">	   
-	    	<div class="tab-content">	
-
+	    	<div class="tab-content">		 
 	    		<div id="mapa-home" class="tab-pane fade in active">
 			      <div class="panel panel-default">
 					  <div class="panel-heading"><h3>Información <small>powered by Fusion Tables</small> </h3></div>
@@ -57,13 +56,56 @@ Mapa de la Ramada <div class="hidden-md hidden-lg"></div>  <small>Información d
 			        @foreach($type->pollutants as $pollutant)
 			        
 
-			        <div id="{{$pollutant->name}}" class="tab-pane fade">
-			        	<div class="panel panel-default">
-			        		<div class="panel-heading"><h3>Información <small>powered by Fusion Tables</small> </h3></div>
-			        		<div class="panel-body">
-			        			{!!$pollutant->iframe!!}
+			        <div id="poll{{$pollutant->id}}" class="tab-pane fade">
+			        	<ul class="nav nav-tabs">
+			        		<li class="active"><a data-toggle="tab" href="#{{$pollutant->id}}maps">Mapas {{$pollutant->name}}</a></li>
+			        		<li><a data-toggle="tab" href="#{{$pollutant->id}}inform">Información del contaminante</a></li>
+			        	</ul>
+			        	<div class="tab-content">
+			        		<div id="{{$pollutant->id}}maps" class="tab-pane fade in active">
+			        			<div class="panel panel-default">
+			        				<div class="panel-body">
+			        					<ul class="nav nav-pills nav-justified">
+			        						@foreach($pollutant->yearMaps as $year)
+			        						@if ($loop->first)
+			        						<li class="active"><a href="#y{{$pollutant->id}}{{$year->year}}" data-toggle="tab">{{$year->year}}</a></li>
+			        						@else
+			        					    <li><a href="#y{{$pollutant->id}}{{$year->year}}" data-toggle="tab" >{{$year->year}}</a></li>
+			        					    @endif
+			        					    @endforeach
+			        					    
+			        						
+			        					</ul>	
+			        					<div class="tab-content">
+			        					    @foreach($pollutant->yearMaps as $year)
+			        						@if ($loop->first)
+			        						<div id="y{{$pollutant->id}}{{$year->year}}" class="tab-pane fade in active">
+			        						{!!$year->iframe!!}	
+			        						</div>
+			        						@else
+			        					    <div id="y{{$pollutant->id}}{{$year->year}}" class="tab-pane fade">
+			        						{!!$year->iframe!!}	
+			        						</div>
+			        					    @endif
+			        					    @endforeach
+			        						
+			        					</div>
+
+
+			        				</div>
+			        			</div>
+			        		</div>
+			        		<div id="{{$pollutant->id}}inform" class="tab-pane fade in">
+			        			<div class="panel panel-default">
+			        				<div class="panel-body">
+			        				<h2>{{$pollutant->name}}</h2>
+			        					{!!$pollutant->description!!}
+			        				</div>
+			        			</div>
 			        		</div>
 			        	</div>
+    
+			        	
 			        </div>
 			        
 			        @endforeach
